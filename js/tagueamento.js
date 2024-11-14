1aeab5e85c76e8bb166fad893620bc2356c73216
eliza-gabriela <161747069+eliza-gabriela@users.noreply.github.com>
2024-11-14 20:46:03 -0300

    Update tagueamento.js

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -4,3 +4,17 @@
 // O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+//EVENTO GA4 Formulário Enviado com sucesso
+document.addEventListener('DOMContentLoaded', function () {
+    var formEnviado = "Obrigado pelo seu contato!";
+    function mensagemDeSucesso() {
+        if (document.body && document.body.innerText.includes(formEnviado)) {
+            console.log("Formulário Enviado");
+            gtag('event', 'formulario_enviado')
+        } else {
+            setTimeout(mensagemDeSucesso, 500);
+        }
+    }
+    mensagemDeSucesso()
+})
